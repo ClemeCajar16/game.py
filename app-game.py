5b846a889f027c83ad4661c574a32bce0ad3aeed
cleme <ccajarlaguna@gmail.com>
2025-07-17 18:15:11 -0500

    se ha creado la vetana

--- app-game.py
+++ app-game.py
@@ -1,2 +1,13 @@
 import pygame
 
+pygame.init() 
+
+ventana = pygame.display.set_mode((ancho, alto))
+
+run = True
+while run: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
