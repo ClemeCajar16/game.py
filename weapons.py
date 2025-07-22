2c037c9ba967629851614661dbdb11d3a90791b9
cleme <ccajarlaguna@gmail.com>
2025-07-22 17:31:37 -0500

    se ha actualizado la movilidad del arma

--- weapons.py
+++ weapons.py
@@ -12,9 +12,23 @@ class Weapon():
 
     def update(self, personaje):
         self.forma.center = personaje.forma.center
-        self.forma.x += personaje.forma.width / 2
+        if personaje.flip == False:
+            self.forma.x += personaje.forma.width / 2
+        if personaje.flip == True:
+            self.forma.x -= personaje.forma.width / 2
+
+    def rotar(self, rotar):
+        if rotar == True: 
+            imagen_flip = pygame.transform.flip(self.image_original, flip_x=True, flip_y=False)
+            self.imagen = pygame.transform.rotate(imagen_flip, self.angulo)
+
+        else:
+            imagen_flip = pygame.transform.flip(self.image_original, flip_x=False, flip_y=False)
+            self.imagen = pygame.transform.rotate(imagen_flip, self.angulo)
 
 
     def dibujar(self, interfaz):
         interfaz.blit(self.imagen, self.forma)
         # pygame.draw.rect(interfaz,constantes.COLOR_ARMA , self.forma, width=2)
+
+    
